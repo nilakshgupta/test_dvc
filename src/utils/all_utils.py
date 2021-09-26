67c0fbf08df2a94a20ac4fbde9400ba7199b8b3b
Nilaksh Gupta <nilakshgupta@gmail.com>
2021-09-26 00:36:39 -0400

    dvc yaml added

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -1,10 +1,13 @@
 import yaml
 import os
 
-def read_yaml(path_to_yaml:str) -> dict:
-    with open (path_to_yaml) as yaml_file:
+
+def read_yaml(path_to_yaml: str) -> dict:
+    with open(path_to_yaml) as yaml_file:
         content = yaml.safe_load(yaml_file)
+
     return content
+
 
 def create_directory(dirs: list):
     for dir_path in dirs:
